037799986f77ed146d8305841192fae4a3a1e7fb
Cachumbin <simoncarreno2006@gmail.com>
2024-11-11 00:07:59 -0500

    feat: Create firebaseConfig file with api key

---
+++ components/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
+  measurementId: process.env.VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { auth, firestore, storage };
